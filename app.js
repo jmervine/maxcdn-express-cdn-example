9a59f6792535fbe6eafc6488bbe8ad1422a6a4c8
Josh Mervine <jmervine@yp.com>
2014-04-16 21:26:30 -0700

    Updates for pending MaxCDN blog post.

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
+'use strict';
+var path = require('path');
 var express = require('express');
-var path    = require('path');
-var app     = express();
+var app = express();
 
-// Set the CDN options
+// Setup options for
 var options = {
     publicDir  : path.join(__dirname, 'public'),
     viewsDir   : path.join(__dirname, 'views'),
     domain     : 'yours.netdna-cdn.com',
-    bucket     : 'your_backet',
+    bucket     : 'your_bucket',
     key        : 'aws_key',
     secret     : 'aws_secret',
     hostname   : 'localhost',
@@ -19,17 +20,17 @@ var options = {
 // Initialize the CDN magic
 var CDN = require('express-cdn')(app, options);
 
+// NOTE: This is the configure block that already exists in your code.
+//       It's shown for placement context.
 app.configure(function() {
     app.set('view engine', 'jade');
-    app.set('view options', { layout: false, pretty: true });
-    app.enable('view cache');
-    app.use(express.bodyParser());
+    app.set('view options', { layout: false });
     app.use(express.static(path.join(__dirname, 'public')));
 });
 
 app.locals({ CDN: CDN() });
 
-app.get('/', function(req, res, next) {
+app.get('*', function(req, res, next) {
     return res.render('index');
 });
 
--- public/script.js
+++ public/script.js
@@ -1,4 +1,10 @@
-function test() {
-    return '&nbsp;&nbsp;&nbsp;This was written by script.js';
-}
-
+/***
+ * scipt test content
+ */
+function setSubHeading() {
+    var h2 = document.getElementsByClassName('subhead').item(this);
+    var subhead = window.location.pathname;
+    if (subhead && subhead !== '/') {
+        h2.innerHTML = '&gt; '+subhead;
+    }
+};
